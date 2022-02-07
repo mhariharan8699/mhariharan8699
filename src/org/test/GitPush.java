243d38ffdfd7d6dd18f215bbec3e1a1e409b487e
mhariharan8699 <mhariharan8699>
2022-02-07 13:04:32 +0530

    message

---
+++ src/org/test/GitPush.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class GitPush {
+	public void add() {
+		System.out.println("git class");
+
+	}
+public static void main(String[] args) {
+	GitPush g=new GitPush();
+	g.add();
+}
+}
